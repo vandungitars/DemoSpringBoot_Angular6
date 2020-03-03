32ac2fe230cdbbf34b70dbbea3017bcc7a0f094c
Lê Văn Dũng <dunglv@rabiloo.com.vn>
2020-03-03 11:04:06 +0700

    up full code

---
+++ src/main/java/com/vandung/DemoAngular6SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vandung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAngular6SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAngular6SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vandung/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.vandung.repository;
+
+import com.vandung.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
